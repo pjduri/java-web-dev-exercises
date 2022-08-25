748c74ef32affbeaf5c109f368c7e281c360e70d
Your Name <pjduri@gmail.com>
2022-08-25 14:13:09 -0500

    Chapter 3 Exercise Array List done

---
+++ src/exercises/exercises/controlflowandcollections/ArrayPractice.java
@@ -0,0 +1,25 @@
+package exercises.exercises.controlflowandcollections;
+
+import java.util.Arrays;
+
+public class ArrayPractice {
+
+    public static void main(String[] args) {
+        int[] integerArr = {1, 1, 2, 3, 5, 8};
+        for (int integer : integerArr) {
+            if (!(integer % 2 == 0)) {
+                System.out.println(integer);
+            }
+        }
+
+        String catHatQuote = "I would not, could not, in a box. " +
+                "I would not, could not with a fox. " +
+                "I will not eat them in a house. I will " +
+                "not eat them with a mouse.";
+        String[] catHatWordsArr = catHatQuote.split(" ");
+        String[] catHatSentencesArr = catHatQuote.split("\\.");
+        System.out.println(Arrays.toString(catHatWordsArr));
+        System.out.println(Arrays.toString(catHatSentencesArr));
+    }
+
+}
